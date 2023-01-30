605e6a52fc9566bf046cc54e4b889594fa1b70ca
jineeeyeah <skytbr1113@yonsei.ac.kr>
2023-01-30 11:25:53 +0900

    Initial commit

---
+++ Mainly.py
@@ -0,0 +1,22 @@
+import streamlit as st
+
+st.set_page_config(
+	page_title='Streamlit 프로토타입 만들기',
+	page_icon='🎈',
+	layout='wide'
+)
+
+st.text('🎈김예진 Streamlit 프로토타입 만들기')
+
+st.title('📌예진 Python')
+
+st.header('Header(머리글)을 입력하세요.')
+st.subheader('Subheader(세부 머리글)을 입력하세요.')
+
+st.markdown('# H1 #')
+st.markdown('## H2 ##')
+st.markdown('### H3 ###')
+st.markdown('#### H4 ####')
+st.markdown('##### H5 #####')
+st.markdown('###### H6 ######')
+
